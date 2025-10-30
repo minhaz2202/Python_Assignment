4951d9f8a4eec992d760b90920f7be2ce3054af1
minhaz2202 <minhaz.arch@gmail.com>
2025-10-30 23:32:15 +0200

    Update solution 01.py

--- Module 09/solution 01.py
+++ Module 09/solution 01.py
@@ -1,23 +1,20 @@
 class Car:
-    def _init_(self, registration_number, max_speed):
+    def __init__(self, registration_number, max_speed):
         self.registration_number = registration_number
         self.max_speed = max_speed
         self.current_speed = 0
         self.travelled_distance = 0
 
+    def status(self):
+        print(f"Registration Number: {self.registration_number}")
+        print(f"Max Speed: {self.max_speed} km/h")
+        print(f"Current Speed: {self.current_speed} km/h")
+        print(f"Travelled Distance: {self.travelled_distance} km")
 
 def main():
-    # Create a new car with registration number ABC-123 and maximum speed 142 km/h
-    new_car = Car("ABC-123", 142)
-
-    # Print out all properties of the new car
+    new_car = Car("TLS-22", 179)
     print("Car Properties:")
-    print(f"Registration Number: {new_car.registration_number}")
-    print(f"Maximum Speed: {new_car.max_speed} km/h")
-    print(f"Current Speed: {new_car.current_speed} km/h")
-    print(f"Travelled Distance: {new_car.travelled_distance} km")
+    new_car.status()
 
-
-# Run the main program
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
